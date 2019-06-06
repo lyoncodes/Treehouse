c762723501c1f1087baa6b7bcbed3a5d9b5846a4
Michael Lyon <lyoncodes@gmail.com>
2019-06-06 13:53:13 -0700

    builds out remaining example functions

--- rest_apis_express_projectfiles/S1V4_starter_files/app.js
+++ rest_apis_express_projectfiles/S1V4_starter_files/app.js
@@ -1,77 +1,27 @@
 const express = require('express');
 const app = express();
-
-const records = require('./records');
+const routes = require('./routes');
 
 app.use(express.json());
+app.use('/api', routes);
+
 
 // Send a GET request to /quotes READ(view) a list of quotes
-app.get('/quotes', async (req, res) => {
- try{
-  const quotes = await records.getQuotes();
-  res.json(quotes);
- } catch(err){
-    res.json({message: err.message});
- }
-})
-// Send a GET request to /quotes/:id to READ(view) a quote
-app.get('/quotes/:id', async (req, res) => {
-  try{
-   const quote = await records.getQuote(req.params.id);
-   if (quote){
-    res.json(quote); 
-   } else {
-    res.status(404).json({message: "quote not found"})
-   }
-
-  } catch(err){
-     res.status(500).json({message: err.message});
-  }
-})
-// Send a POST request to /quotes to CREATE a new quote
-app.post('/quotes', async (req, res) => {
- try {
-     if (req.body.author && req.body.quote){
-          const quote = await records.createQuote({
-              quote: req.body.quote,
-              author: req.body.author    
-          }); 
-          res.status(201).json(quote);
-   } else {
-          res.status(400).json({message: "Quote and author required"})
-   }
-  } catch(err) {
-      res.status(500).json({message: err.message})
-  }
-});
-// Send a PUT request to /quotes/:id to UPDATE (edit) a quote
-app.put('/quotes/:id', async (req, res) => {
-  try {
-     const quote = await records.getQuote(req.params.id);
-     if (quote){
-          quote.body = req.body.quote;
-          quote.author = req.body.author;
-
-          await records.updateQuote(quote);
-          res.status(204).end();
-     } else {
-        res.status(404).json({message: "resource not located"});
-     }
-  } catch(err) {
-     res.status(500).json({message: err.message})
-  }
-})
-// Send a DELETE request to /quotes/:id to DELETE a quote
-app.delete('/quotes/:id', async (req, res) => {
- try {
-   const quote = await records.getQuote(req.params.id);
-     await records.deleteQuote(quote);
-     res.status(204).end();
-     
- } catch(err){
-     res.status(500).json({message: err.message})
- }
-})
 // Send a GET request to /quotes/quotes/random to READ(view) a random quote
+
+app.use((req, res, next) => {
+    const err = new Error("Not Found");
+    err.status = 404;
+    next(err);
+})
+
+app.use((err, req, res, next) => {
+    res.status(err.status || (500));
+    res.json({
+     error: {
+       message: err.message
+     }
+    })
+})
 
 app.listen(3000, () => console.log('Quote API listening on port 3000!'));
