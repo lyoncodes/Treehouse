64420d5895f506c31008e6d889fddf3208c174d2
Michael Lyon <lyoncodes@gmail.com>
2019-07-16 15:47:58 -0700

    adds dynamic circles in response to stat arrays

--- d3example/public/js/foo.js
+++ d3example/public/js/foo.js
@@ -5,3 +5,10 @@ export function hello() {
 export function add(x, y) {
   return x + y;
 }
+
+export function calculateTeamAverages(player, team){
+  let ppg = (player[0] / team[0]) * 100;
+  let reb = (player[1] / team[1]) * 100;
+  let ast = (player[2] / team [2]) * 100;
+  return [ppg, reb, ast]
+}
