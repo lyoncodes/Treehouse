3d6bd212f17c64b889e81836f7cc04e8f2927715
Michael Lyon <lyoncodes@gmail.com>
2019-07-20 15:52:46 -0700

    adds advanced stats functions

---
+++ d3example/public/js/barchart.js
@@ -0,0 +1,65 @@
+const minData = [
+  {
+    "player":"Lillard",
+    "min":35
+  },
+  {
+    "player":"McCollum",
+    "min":34
+  },
+  {
+    "player":"Aminu",
+    "min":28
+  },
+  {
+    "player":"Nurkic",
+    "min":27
+  },
+  {
+    "player":"Hood",
+    "min":24
+  },
+]
+
+export function barchart(){
+  let svgContainer = d3.select("#barChart");
+
+  // Data Specific & Layout Values
+  const xValue = d => d.min;
+  const yValue = d => d.player;
+  const margin = {top: 20, right: 10, bottom: 20, left: 80}
+
+  const width = +svgContainer._groups[0][0].width.baseVal.value;
+  const height = +svgContainer._groups[0][0].height.baseVal.value;
+
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
+
+  // X scale
+  const xScale = d3.scaleLinear()
+    .domain([0, 40])
+    .range([0, innerWidth]);
+  
+  // Y scale
+  const yScale = d3.scaleBand()
+    .domain(minData.map(yValue))
+    .range([0, innerHeight])
+    .padding(0.1);
+  
+  // Group element
+  const g = svgContainer.append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+  // Y Axis & X Axis
+  g.append('g').call(d3.axisLeft(yScale));
+  g.append('g').call(d3.axisBottom(xScale))
+    .attr('transform', `translate(0,${innerHeight})`);
+
+  let rect = g.selectAll("rect")
+    .data(minData)
+    .enter().append('rect')
+      .attr('y', d => yScale(yValue(d)))
+      .attr('width', d => xScale(xValue(d)))
+      .attr('height', yScale.bandwidth());
+  
+}
--- d3example/public/js/foo.js
+++ d3example/public/js/foo.js
@@ -1,14 +1,38 @@
 export function hello() {
   return "ALL GOOD TO GO";
-}
+};
 
 export function add(x, y) {
   return x + y;
-}
+};
 
-export function calculateTeamAverages(player, team){
+export function calcTeamAverages(player, team){
   let ppg = (player[0] / team[0]) * 100;
   let reb = (player[1] / team[1]) * 100;
   let ast = (player[2] / team [2]) * 100;
   return [ppg, reb, ast]
+};
+
+export function calcUSG(fga, fta, tov, teamMin, min, teamFga, teamFta, teamTov){
+  let usg = 100 * ((fga + 0.44 * fta + tov)*(teamMin/5))/(min*(teamFga + 0.44 * teamFta + teamTov));
+  console.log ('usage: ' + usg);
+  return usg;
+};
+
+export function calcAP(ast, fg, min, teamMin, teamFg){
+  let astPer = 100*ast/(((min/(teamMin/5))*teamFg)-fg);
+  console.log('assist %: ' + astPer);
+  return astPer;
+};
+
+export function calcEFG(fg, tpfg, fga){
+  let efg = (fg + 0.5 * tpfg)/fga;
+  console.log('effective fg: ' + efg);
+  return efg;
+}
+
+export function calcTSP(pts, fga, fta){
+  let tsp = pts/(2*(fga + 0.44 * fta))
+  console.log('true shooting %: ' + tsp);
+  return tsp;
 }
